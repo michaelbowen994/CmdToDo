69669e2d18cb52c4a88c8a5290524099b656b324
michaelbowen <michael.bowen994@gmail.com>
2025-01-12 13:24:43 -0600

    Initial build of project backend with spring boot

---
+++ cmdtodo/src/main/java/com/michaelbowen/cmdtodo/CmdtodoApplication.java
@@ -0,0 +1,13 @@
+package com.michaelbowen.cmdtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmdtodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmdtodoApplication.class, args);
+	}
+
+}
---
+++ cmdtodo/src/test/java/com/michaelbowen/cmdtodo/CmdtodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelbowen.cmdtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmdtodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
